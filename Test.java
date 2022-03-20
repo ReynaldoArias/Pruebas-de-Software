c43c7c401f3c30296a85704294b225f20348a024
ReynaldoArias <101991829+ReynaldoArias@users.noreply.github.com>
2022-03-20 13:51:06 -0500

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Test;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		Rules rules=new Rules();
+		rules.GameStart();
+	}
+}
